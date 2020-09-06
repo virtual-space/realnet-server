2b25886ab2b7ecd36b0856f4fb10d7a4317ca71e
marko <marko.laban@l33tsystems.com>
2020-09-06 18:21:10 +1200

    Disabling bluetooth

--- realnet_server/bluetooth_items.py
+++ realnet_server/bluetooth_items.py
@@ -1,18 +1,20 @@
 from .item import Item
 from .bluetooth_item import BluetoothItem
 
-import bluetooth
+# import bluetooth
 
 
 class BluetoothItems(Item):
 
     def get_items(self):
+        return []
         items = []
         for device in bluetooth.discover_devices(lookup_names=True):
             items.append(BluetoothItem(device))
         return items
 
     def get_item(self, id):
+        return None
         for device in bluetooth.discover_devices(lookup_names=True):
             item = BluetoothItem(device)
             if id == item.get_id():
