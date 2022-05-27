ed792152121cd506a3be41cd137eaade10716351
Samuel-Boyes-l33t <sam.boyes@l33tsystems.com>
2022-05-27 13:21:30 +1200

    [RS-99] Clean up orgs delete item.

--- realnet_server/modules/orgs.py
+++ realnet_server/modules/orgs.py
@@ -544,37 +544,26 @@ class Orgs(Default):
     def delete_item(self, item):
         ids = item.id.split('_')
         length = len(ids)
-        #print("id list", ids)
         rows_to_delete = []
-        if length == 1:
-            pass #attempting to delete the 'org' module. This should raise an error.
-        elif length == 2:
-            pass #attempting to delete an org. This should raise an error.
-        elif length == 3:
-            #THIS WILL DELETE FROM BOTH TABLES IF THE ID IS IN BOTH.
-            #attempting to delete a group, all accounts and all roles linked to it.
-            #and/or delete a client.
-            group_id = ids[-1]
-            rows_to_delete.append(Group.query.filter(Group.id == group_id).first())
-            for row in Account.query.filter(Account.group_id == group_id):
-                rows_to_delete.append(row)
-            for row in Item.query.filter(Item.group_id == group_id):
-                rows_to_delete.append(row)
-            for row in AccountGroup.query.filter(AccountGroup.group_id == group_id):
-                rows_to_delete.append(row)
-            rows_to_delete.append(Client.query.filter(Client.id == group_id).first())
+        if length == 3:
+            target_id = ids[-1]
+            if item.type.name == 'Group':
+                rows_to_delete.append(Group.query.filter(Group.id == target_id).first())
+                for row in Account.query.filter(Account.group_id == target_id):
+                    rows_to_delete.append(row)
+                for row in Item.query.filter(Item.group_id == target_id):
+                    rows_to_delete.append(row)
+                for row in AccountGroup.query.filter(AccountGroup.group_id == target_id):
+                    rows_to_delete.append(row)
+            elif item.type.name == 'Client':
+                rows_to_delete.append(Client.query.filter(Client.id == target_id).first())
         elif length == 4:
-            #attempting to delete a account in a group, and all roles linked to that account
-            account_id = ids[-1]
-            rows_to_delete.append(Account.query.filter(Account.id == account_id).first())
-            rows_to_delete.append(AccountGroup.query.filter(AccountGroup.account_id == account_id).first())
-            for row in Item.query.filter(Item.owner_id == account_id):
-                rows_to_delete.append(row)
-        elif length == 5:
-            pass #attempting to delete a role in an account
-            #item_id = ids[-1]
-            #rows_to_delete.append(Item.query.filter(Item.id == item_id).first)
-        #print(rows_to_delete)
+            if item.type.name == 'Account':
+                account_id = ids[-1]
+                rows_to_delete.append(Account.query.filter(Account.id == account_id).first())
+                rows_to_delete.append(AccountGroup.query.filter(AccountGroup.account_id == account_id).first())
+                for row in Item.query.filter(Item.owner_id == account_id):
+                    rows_to_delete.append(row)
         for row in rows_to_delete:
             if(row is not None):
                 db.session.delete(row)
